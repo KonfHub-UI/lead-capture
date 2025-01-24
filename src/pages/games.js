1058ca9c0eefa91be491f7e88959519321c287b3
asit <asit@konfhub.com>
2025-01-24 17:11:34 +0530

    game list page completed

--- src/pages/games.js
+++ src/pages/games.js
@@ -1,7 +1,15 @@
-const Games = () =>{
-    return(
-        <div>Games</div>
-    )
-}
+import "./games/games.css";
+
+const Games = () => {
+  return (
+    <div className="game-list-root">
+      <h1 className="heading">Hi John Doe</h1>
+      <p className="subheading">Select the game that you want to play</p>
+      <div className="button-container">
+        <button className="game-text-box">Word scramble</button>
+      </div>
+    </div>
+  );
+};
 
 export default Games;
