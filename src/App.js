6f4ff052c2e85514546e281cf4a3d41a37cb345f
asit <asit@konfhub.com>
2025-01-24 22:12:16 +0530

    refactor: clean up imports and update Router basename

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Router, Navigate, Route, Routes, useLocation } from "react-router-dom";
+import React from "react";
+import "./App.css";
+import {
+  Route,
+  Routes,
+} from "react-router-dom";
 import SignIn from "./pages/singIn";
 import Games from "./pages/games/games";
 import WordScramble from "./pages/games/word-scrumble";
-import SuccessPage from './pages/success/SuccessPage';
-
+import SuccessPage from "./pages/success/SuccessPage";
 
 function App() {
   return (
-            <Routes>
-                <Route path="/" element={<SignIn />} />
-                <Route path="/games" element={<Games />} />
-                <Route path="/games/word-scramble" element={<WordScramble />} />
-                <Route path="/success" element={<SuccessPage />} />
-            </Routes>
+    <Routes>
+      <Route path="/" element={<SignIn />} />
+      <Route path="/games" element={<Games />} />
+      <Route path="/games/word-scramble" element={<WordScramble />} />
+      <Route path="/success" element={<SuccessPage />} />
+    </Routes>
   );
 }
 
