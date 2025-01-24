7496e5805f11ba42dc95361981483cbc7da7bd6e
asit <asit@konfhub.com>
2025-01-24 19:37:29 +0530

    updated

--- src/pages/success/SuccessPage.js
+++ src/pages/success/SuccessPage.js
@@ -3,10 +3,9 @@ import "./success.css";
 const SuccessPage = () => {
   return (
     <div className="success-page-root">
-      <h1 className="heading-success">You Win!</h1>
+      <h1 className="heading-success">Thank you for participating</h1>
       <div className="button-container">
-        <button className="game-text-box">Your Score</button>
-        <button className="game-text-box">1200</button>
+        <button className="game-text-box">Congrats</button>
       </div>
     </div>
   );
